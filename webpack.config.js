22941f141a2d7cd6682317b23f4913c14b6aebe2
Damian Rutkowski <drutk@softserveinc.com>
2017-07-11 16:16:36 +0300

    Webpack with scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/app.js',
+    context:
+        path.resolve(__dirname, 'src'),
+    entry:
+        './app.js',
     output: {
-        filename: 'dist/app.js '
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: [
+                'babel-loader'
+            ]
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
     }
-}
+};
