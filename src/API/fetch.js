82034e555664e207d106db80c15cad45342f3adb
Lukasz Wysocki <lwyso@softserveinc.com>
2017-07-13 09:28:19 +0200

    Fetching API and load leauge name

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -1,12 +1,16 @@
-function loadDoc(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://api.football-data.org/v1/competitions", false);
-    xhr.setRequestHeader("X-Auth-Token", "63e07dbf62ac463d9aa94a83443512ec");
-    xhr.send();
-    var data = xhr.responseText;
-    var jasonResponse = JSON.parse(data);
-    for(var i = 0; jasonResponse.length > i; i++){
-        console.log(jasonResponse[i].caption);
-    }
-    
+function loadLeauge(){
+    const url = 'http://api.football-data.org/v1/competitions';
+    var request = new Request(url, {
+        method: 'GET',
+        headers: {
+            "X-Auth-Token" : "63e07dbf62ac463d9aa94a83443512ec"
+        }
+    });
+    fetch(request).then(function(response){
+        return response.json();
+    }).then(function(j){
+        for(var i = 0; i < j.length;  i++){
+            console.log(j[i].caption);
+        }
+    });
 }
