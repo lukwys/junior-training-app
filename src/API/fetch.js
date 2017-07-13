092e92cf006d6194bcee97a10acd68308f2a039f
Lukasz Wysocki <lwyso@softserveinc.com>
2017-07-13 13:59:56 +0200

    clean leauge load function

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -1,6 +1,5 @@
-function loadLeauge(token){
-    //var token = "63e07dbf62ac463d9aa94a83443512ec"
-    request = new Request('http://api.football-data.org/v1/competitions', {
+export default function loadLeauge(token, url){
+    const request = new Request(url, {
         method: 'GET',
         headers: {
             "X-Auth-Token" : token
@@ -8,11 +7,8 @@ function loadLeauge(token){
     });
     
     fetch(request)
-    .then(response => {
-        return response.json();
-    })
-    .then(j => {
-        j.map(i => console.log(i.caption));
-        }
-    );
+      .then(response => response.json())
+      .then(j => {
+          const list = j.map(i => i.caption);
+      });
 }
---
+++ src/app.js
@@ -0,0 +1,3 @@
+import loadLeague from "./api/fetch"
+
+loadLeague("63e07dbf62ac463d9aa94a83443512ec", "http://api.football-data.org/v1/competitions");
