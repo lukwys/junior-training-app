e99c3ea5501816060bd5ac8bc8d0ca3ef8477963
Txwxcc <drutk@softserveinc.com>
2017-07-26 09:55:21 +0200

    add husky

--- src/render/searchResult.js
+++ src/render/searchResult.js
@@ -1,6 +1,6 @@
 export default function renderSearchResult(results) {
     const headers = Object.keys(results).map(key => `<th>${key}</th>`);
-    const values = Object.keys(results).map(key => `<th>${results[key]}</th>`);
+    const values = Object.keys(results).map(key => `<td>${results[key]}</td>`);
 
     return `<thead><tr>${headers.join('')}</tr></thead>` +
         `<tbody><tr>${values.join('')}</tr></tbody>`;
