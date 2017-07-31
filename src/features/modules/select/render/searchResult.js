a36320dcbdbae92b60db4e9cc6758b68797129e0
lukwys <lukasz-wysocki18@wp.pl>
2017-07-31 15:36:28 +0200

    format code (+4 squashed commit)
    Squashed commit:
    
    [e25569d] remove errors
    
    [60441a3] Add react component to searchResult
    
    [2087cfc] Add react component to searchResult
    
    [fb77fba] Add react

--- src/features/modules/select/render/searchResult.js
+++ src/features/modules/select/render/searchResult.js
@@ -1,13 +1,32 @@
+import React from 'react';
 /**
  * generates table with results in html
  * @param {Object} results object that contains table elements
  * @return string with html table tags that contains searched results
  */
-
 export default function renderSearchResult(results) {
-    const headers = Object.keys(results).map(key => `<th>${key}</th>`);
-    const values = Object.keys(results).map(key => `<td>${results[key]}</td>`);
+    // eslint-disable-next-line
+    class TableRows extends React.Component {
+        // eslint-disable-next-line
+        constructor(props) {
+            super(props);
+        }
+        render() {
+            const { data } = this.props;
+            const headers = Object.keys(data).map(key =>
+                <th>${key}</th>
+            );
+            const values = Object.keys(data).map(key =>
+                <th>${data[key]}</th>
+            );
+            return (
+                <table>
+                    <tr>{headers}</tr>
+                    <tr>{values}</tr>
+                </table>
+            );
+        }
+    }
 
-    return `<thead><tr>${headers.join('')}</tr></thead>` +
-        `<tbody><tr>${values.join('')}</tr></tbody>`;
+    return <TableRows data={results} />;
 }
