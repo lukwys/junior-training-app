e4e4ef63c5dde17f1ee800998424b3db331fc5e9
Txwxcc <drutk@softserveinc.com>
2017-08-02 12:38:18 +0200

    change signature of source/handler/select function

--- src/features/modules/source/bootstrap.js
+++ src/features/modules/source/bootstrap.js
@@ -3,5 +3,5 @@ import config from './source.conf';
 import api from './api.conf';
 
 export default function bootstrap() {
-    handler.select(config.inputs, config.dataAttr, config.sources, config.order, config.template, api.sources);
+    handler.select(config, api.sources);
 }
